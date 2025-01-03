23bbfe3fe0f5eae7cda4656a32836f2cc3676b0f
Folorunso Timson <timsonfoli@gmail.com>
2025-01-03 01:47:55 +0100

    first commit towards mobile

---
+++ gettingStarted.dart
@@ -0,0 +1,18 @@
+void main(){
+    String firstName;
+    String lastName;
+    int age;
+    double height;
+
+    firstName= "Folorunso";
+    lastName = "Timson";
+    age = 35;
+    height = 1.78;
+
+    print("First Name: $firstName");
+    print("Last Name: $lastName");
+    print("Age: $age");
+    print("Height: $height meters");
+
+
+}
